c8e775f74891fbb52f96d38493c4a1fb3a965009
Ruben Paz <me@ruben.io>
2016-02-15 00:09:17 +0100

    reverse list algorithm

--- lib/listutls.ex
+++ lib/listutls.ex
@@ -1,5 +1,19 @@
 defmodule ListUtls do
-  
+
+  ## Returns a list with the contents reversed
+  def reverse(list), do: reverse(list, [])
+
+  defp reverse([], acc), do: acc
+  defp reverse([head | tail], acc) do
+    reverse tail, [head | acc]
+  end
+
+
+  ## Flattens a list
+  def flatten(list) do
+    reverse(flatten(list, []))
+  end
+
   defp flatten([], acc), do: acc
 
   defp flatten([head | tail], acc) when is_list(head) do
@@ -8,10 +22,6 @@ defmodule ListUtls do
 
   defp flatten([head | tail], acc) do
     flatten(tail, [head | acc])
-  end
-
-  def flatten(list) do
-    Enum.reverse(flatten(list, []))
   end
 
 end
--- test/lisutls_test.exs
+++ test/lisutls_test.exs
@@ -2,6 +2,11 @@ defmodule ListUtlsTest do
   use ExUnit.Case
   doctest ListUtls
   import ListUtls
+  
+  test "reverse" do
+    assert reverse([]) == []
+    assert reverse([1,2,3,4,5]) == [5, 4, 3, 2, 1]
+  end
 
   test "flatten" do
     assert flatten([]) == []
